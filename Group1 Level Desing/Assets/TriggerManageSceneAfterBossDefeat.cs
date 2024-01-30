9d8ba461498010a7e5c03a83a2aa232b21bdc198
Enrique Gutierrez Jimenez <kikeguji7@gmail.com>
2024-01-30 04:01:34 +0100

    Laia model, lights and explosions/particles

--- Group1 Level Desing/Assets/TriggerManageSceneAfterBossDefeat.cs
+++ Group1 Level Desing/Assets/TriggerManageSceneAfterBossDefeat.cs
@@ -7,6 +7,7 @@ public class TriggerManageSceneAfterBossDefeat : MonoBehaviour
 
     public Collider blockAccesToRoom2;
     public Collider blockAccesToRoom5;
+    public GameObject activeParticle;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +24,7 @@ public class TriggerManageSceneAfterBossDefeat : MonoBehaviour
     {
         if (collider.tag == "Player")
         {
+            activeParticle.SetActive(true);
             blockAccesToRoom2.enabled = true;
             blockAccesToRoom5.enabled = true;
         }
